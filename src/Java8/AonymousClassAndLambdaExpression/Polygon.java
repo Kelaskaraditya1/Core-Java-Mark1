5d558245c43575a9942c066a55fe65d93bf23445
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2024-12-22 10:36:59 +0530

    Stupid Commit

--- src/Java8/AonymousClassAndLambdaExpression/Polygon.java
+++ src/Java8/AonymousClassAndLambdaExpression/Polygon.java
@@ -5,30 +5,39 @@ public class Polygon {
     public Polygon(int sides){
         this.sides=sides;
     }
+
     public void getSides(){
         System.out.println("There are "+this.sides+" no of sides of Polygon");
     }
+
     public class Triangle{
         public int height,base;
+
         public Triangle(int height,int base){
             this.base=base;
             this.height=height;
+
         }
         public double getArea(Triangle triangle){
             return (0.5*triangle.base*triangle.base);
         }
+
         public int getPerimeter(Triangle triangle){
             return 2*triangle.base+triangle.height;
         }
     }
+
     public static class EquilateralTriangle{
         int length;
+
         public EquilateralTriangle(int length){
             this.length=length;
         }
+
         public double getArea(EquilateralTriangle triangle){
             return (1.732*Math.pow(triangle.length,2.0));
         }
+
         public int getPerimeter(EquilateralTriangle triangle){
             return (3*length);
         }
